edebfccbf763c9991490086df3dd111005e4bc1f
sagarptil <sgrptil2004@gmail.com>
2025-08-06 10:47:58 +0530

    Add files via upload

---
+++ VolunteerRegistration.sql
@@ -0,0 +1,9 @@
+create database volunteerregistration;
+use volunteerregistration;
+
+create table volunteerform(name varchar(50),email varchar(50),phone varchar(50),age varchar(50),gender varchar(50),
+address varchar(50),skill varchar(50),availability varchar(50));
+
+select * from volunteerform;
+
+drop table volunteerform;
